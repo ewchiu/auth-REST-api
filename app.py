d70fbf87684361b8a06ff6a93e76ac521a2c8b55
Eric Chiu <ericwchiu@gmail.com>
2021-11-11 22:34:51 -0800

    updated required dependencies

--- app.py
+++ app.py
@@ -202,6 +202,6 @@ if __name__ == '__main__':
 	# When running locally, disable OAuthlib's HTTPs verification.
 	# ACTION ITEM for developers:
 	#     When running in production *do not* leave this option enabled.
-	os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+	# os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-	app.run(host='127.0.0.1', port=8080, debug=True)
+	app.run(host='0.0.0.0', debug=True)
